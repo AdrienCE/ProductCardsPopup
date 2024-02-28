4c16c20ea601815be7e51790119bf6c774fd7147
Adrien <adrien.cenee@outlook.fr>
2024-02-28 17:56:09 +0100

    mis a jour couleurs de la page + correction affichage image 4

---
+++ script.js
@@ -0,0 +1,46 @@
+
+let previewContainer = document.querySelector('.products-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product => {
+  product.onclick = () => {
+    previewContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+
+    previewBox.forEach(preview => {
+
+      let target = preview.getAttribute('data-target');
+
+      if (name == target) {
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {    
+    close.classList.remove('active');   
+    previewContainer.style.display = 'none';
+  };
+});
+
+
+document.querySelector('.cart-container').onclick = () => {  
+  document.querySelector('.cart-container').style.display = 'none';
+};
+
+
+document.querySelectorAll('.products-container .cart').forEach(cartButton => {
+  cartButton.onclick = (e) => {    
+    event.preventDefault(); 
+    document.querySelector('.cart-container').style.display = 'block';
+  };
+});
+
+document.querySelectorAll('.products-container .cart').forEach(cartButton => {
+  cartButton.onclick = (e) => {
+    event.preventDefault();
+    document.querySelector('.cart-container').style.display = 'block';
+  };
+});
